3b13f867a520118e14e1681afd69fcceb35f3ef6
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-10-11 09:31:32 +0300

    memory annealer

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    private static void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean : gcbeans) {
+            System.out.println("GC name:" + gcbean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = (notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+                }
+            };
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        switchOnMonitoring();
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 1_000_000; i++) {
+            Thread.sleep(100);
+            for (int j = 0; j < 1_000_000; j++) {
+                list.add(j);
+            }
+        }
+    }
+
+}
